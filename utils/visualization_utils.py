e92e9961b819c2e7354cc6519748cefadeaeb7bf
S@n1X <s4nixd@gmail.com>
2020-02-12 20:08:23 +0100

    Update the return of visualize_bo.. t get x,y coords

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -750,7 +750,7 @@ def visualize_boxes_and_labels_on_image_array(
           radius=line_thickness / 2,
           use_normalized_coordinates=use_normalized_coordinates)
 
-  return image
+  return image, [ymin, xmin, ymax, xmax]
 
 
 def add_cdf_image_summary(values, name):
